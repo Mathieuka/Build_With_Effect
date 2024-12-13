7e93f4f61378a62195d59a6acba93620c8fc671c
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-12-13 13:28:20 +0100

    feat: Refactor CreateReelUseCase to integrate ReelRepository

--- src/core/usecases/__tests__/CreateReelUseCase.spec.ts
+++ src/core/usecases/__tests__/CreateReelUseCase.spec.ts
@@ -1,17 +1,27 @@
 import { describe, expect, test } from "vitest";
 import CreateReelUseCase from "../createReelUseCase";
 import { Effect } from "effect";
+import ReelRepository from "../../domain/repositories/ReelRepository";
+import InMemoryReelRepository from "../../../infra/InMemoryReelRepository";
 
 describe("CreateReelUseCase", () => {
   test("Create a new reel", () => {
-    const createReelUseCase = new CreateReelUseCase();
+    const repo: ReelRepository = new InMemoryReelRepository();
+    const createReelUseCase = new CreateReelUseCase(repo);
     const program = createReelUseCase.execute({
       coverage: 100,
       click: 10,
     });
 
-    const reel = Effect.runSync(program);
+    const id = Effect.runSync(program);
 
-    expect(reel.calculateRatio()).toEqual(10);
+    expect(id).toEqual(0);
+
+    expect(repo.list()).toEqual([
+      {
+        click: 10,
+        coverage: 100,
+      },
+    ]);
   });
 });
--- src/core/usecases/createReelUseCase.ts
+++ src/core/usecases/createReelUseCase.ts
@@ -1,5 +1,6 @@
 import { Effect } from "effect";
 import Reel from "../domain";
+import ReelRepository from "../domain/repositories/ReelRepository";
 
 export interface UseCase<Request, Response, Error> {
   execute(request?: Request): Effect.Effect<Response, Error>;
@@ -10,11 +11,13 @@ type CreateReelRequest = {
   click: number;
 };
 
-class CreateReelUseCase implements UseCase<CreateReelRequest, Reel, never> {
-  execute(request: CreateReelRequest): Effect.Effect<Reel> {
+class CreateReelUseCase implements UseCase<CreateReelRequest, number, never> {
+  constructor(private reelRepository: ReelRepository) {}
+
+  execute(request: CreateReelRequest): Effect.Effect<number> {
     const reel = new Reel(request.coverage, request.click);
 
-    return Effect.succeed(reel);
+    return Effect.succeed(this.reelRepository.create(reel));
   }
 }
 
